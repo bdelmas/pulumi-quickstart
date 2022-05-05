faa59e36f840f0879570c013ef0748ca9a5992f7
Benjamin Delmas <bdelmas.pro@gmail.com>
2022-05-05 19:52:40 +0200

    Update index and make it public

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -3,13 +3,26 @@ import * as gcp from "@pulumi/gcp";
 
 // Create a GCP resource (Storage Bucket)
 const bucket = new gcp.storage.Bucket("my-bucket", {
-    location: "US"
+    website: {
+        mainPageSuffix: "index.html"
+    },
+    uniformBucketLevelAccess: true,
+    location: "US",
 });
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
 
-const bucketObject = new gcp.storage.BucketObject("../index.html", {
+const bucketObject = new gcp.storage.BucketObject("index.html", {
     bucket: bucket.name,
-    source: new pulumi.asset.FileAsset("../index.html")
+    contentType: "text/html",
+    source: new pulumi.asset.FileAsset("index.html"),
 });
+
+const bucketIAMBinding = new gcp.storage.BucketIAMBinding("my-bucket-IAMBinding", {
+    bucket: bucket.name,
+    role: "roles/storage.objectViewer",
+    members: ["allUsers"]
+});
+
+export const bucketEndpoint = pulumi.concat("http://storage.googleapis.com/", bucket.name, "/", bucketObject.name);
